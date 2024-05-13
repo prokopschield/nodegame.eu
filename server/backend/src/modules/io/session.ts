fbabee5ac430e6b535d51c9dad7e1f4fd19a4753
Prokop <prokop.schield@protonmail.com>
2024-05-13 23:05:33 +0200

    feat(server/backend): socket session attach/detach

--- server/backend/src/modules/io/session.ts
+++ server/backend/src/modules/io/session.ts
@@ -10,6 +10,24 @@ export class Session {
 		this.id = socket.id;
 		this.socket = socket;
 
+		this.attach(socket);
+	}
+
+	detach() {
+		socket_session_map.delete(this.socket);
+		this.socket.disconnect(true);
+	}
+
+	attach(socket: Socket) {
+		if (socket !== this.socket) {
+			this.detach();
+		}
+
+		if (socket_session_map.has(socket)) {
+			// TODO
+		}
+
+		this.socket = socket;
 		socket_session_map.set(socket, this);
 	}
 }
