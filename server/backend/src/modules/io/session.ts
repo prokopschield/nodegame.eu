7e0b3e46cb6497b2dce1473d561c530e2c90c7e3
Prokop <prokop.schield@protonmail.com>
2024-06-08 00:01:31 +0200

    feat(server/backend/src/modules/io/session.ts): duplicite attach returns error

--- server/backend/src/modules/io/session.ts
+++ server/backend/src/modules/io/session.ts
@@ -1,3 +1,4 @@
+import { Err, Ok, Result } from "ps-std";
 import { Socket } from "socket.io";
 
 export const socket_session_map = new WeakMap<Socket, Session>();
@@ -18,16 +19,22 @@ export class Session {
 		this.socket.disconnect(true);
 	}
 
-	attach(socket: Socket) {
+	attach(socket: Socket): Result<this, string> {
 		if (socket !== this.socket) {
 			this.detach();
 		}
 
 		if (socket_session_map.has(socket)) {
-			// TODO
+			const other = socket_session_map.get(socket);
+
+			if (other !== this) {
+				return Err("SOCKET_ALREADY_ATTACHED");
+			}
 		}
 
 		this.socket = socket;
 		socket_session_map.set(socket, this);
+
+		return Ok(this);
 	}
 }
